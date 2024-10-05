71126b77eeaf8614118f8df7b52dfc3f322ab2cc
Falguni Mondal <career.falguni@gmail.com>
2024-10-05 20:44:55 +0530

    Side Nav Ui completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidenav from './components/Navbar/Sidenav'
+import Routing from './utils/Routing'
 
 const App = () => {
   return (
-    <div className='w-full min-h-screen text-[#e9e9e9]'>
+    <div className='w-full h-screen flex text-[#e9e9e9]'>
       <Sidenav />
+      <Routing />
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='w-82% min-h-screen' id='home-section'>
+        <section className="hero-section">
+            <h1>Hero</h1>
+        </section>
+    </section>
+  )
+}
+
+export default Home
